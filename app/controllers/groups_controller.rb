588c21594f417e27bfd4d0d86dc366df7ee75d2b
moriwakirisako <risakomoriwaki@gmail.com>
2019-07-21 20:05:36 +0900

    アクセス先のビューを作成

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,14 +4,19 @@ class GroupsController < ApplicationController
   end
 
   def create
-    
+    @group = group.new(group_params)
+    end
   end
 
   def edit
+    @group = Gweet.find(params[:id])
   end
 
   def update
+    @group = Group.find(params[:id])
+    if group.user_id == current_user.id
+      group.update(group_params)
+      end
   end
 
-  # コメント
 end
