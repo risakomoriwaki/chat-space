7eef3ff09ffbef762ddc3b39dfe35bcc2e1730bd
moriwakirisako <risakomoriwaki@gmail.com>
2019-07-24 20:52:02 +0900

    messageモデル・ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'groups#inde'
+  root to: 'groups#index'
   resources :users, only: [:edit, :update]
   resources :groups, onli: [:new, :create, :edit, :update] do
     # グループ機能は新規作成new、編集機能edit、グループを保存crate、グループを更新update
